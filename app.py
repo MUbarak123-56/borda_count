2aee517552e9a81458916648107b3815d73cbb66
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2024-10-27 16:09:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 st.set_page_config(page_title="Borda Count Selection")
 st.header("Borda Count Selection System")
 st.write("""This app leverages a Borda Count system to award points to competitors in a ranked order""")
-st.markdown("")
+st.markdown("Here is a **[link](https://en.wikipedia.org/wiki/Borda_count)** to learn more about the Borda Count method.")
 uploaded_data = st.file_uploader("Upload the file containing the rank", type=["csv", "xlsx"])
 
 if uploaded_data is not None:
