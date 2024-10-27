7d93c59677715c19c450a698f3f7aae50f320877
Mubarak Ganiyu <ganiyubaraq@gmail.com>
2024-10-27 15:25:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.set_page_config(page_title="Borda Count Selection")
 st.header("Borda Count Selection System")
 st.write("""This app leverages a Borda Count system to award points to competitors in a ranked order""")
 data = st.file_uploader("Upload the file containing the rank", type=["csv", "xlsx"])
-st.dataframe(data)
+data
